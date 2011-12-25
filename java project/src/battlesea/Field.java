1be8683492cf02572d32afe731c9e728f7197328
volume_8091@mail.ru <volume_8091@mail.ru@3fb16b15-11f6-f6c0-ace2-5d59990fd6ff>
2011-12-25 05:09:19 +0000

    Перевел Field в новый класс, теперь все происходит в main. Первое поле заполняется случайно, за него играет компьютер, а второе поле заполняет человек.

--- java project/src/battlesea/Field.java
+++ java project/src/battlesea/Field.java
@@ -6,102 +6,89 @@ import java.util.Random;
 public class Field
     {
 
-     private static final int SIZE = 7; //const used fo size of the field
+     private static final int SIZE = 7; //const used for size of the field
+     private static final int s = 3;    //quantity of 1x1(small) ships
 
      private static int [][] neighbors;
-     
-     
 
-     public int getSize ()
-    {
+     private int[][] field;
+
+     public Field()
+     {
+         field = new int[SIZE][SIZE];
+         //massive that is used for placing ships(1 is ship, 0 is empty)
+     }
+     
+     public int[][] getField()
+     {
+         return field;
+     }
+
+     public int getSmallShips()
+     {
+         return s;
+     }
+
+     public int getSize()
+     {
          return SIZE;
-    }
+     }
 
-     public static void main(String[] args) throws IOException
+     public void setCell(int row, int col, int value)
+     {
+         field[row][col] = value;
+     }
+
+     public int getCell(int row, int col)
+     {
+         int value = field[row][col];
+         return value;
+     }
+
+     public void fillFieldByRandom() throws IOException
      {
         initNeighbors();
-        int[][] field1 = new int[SIZE][SIZE]; //massive that is used for placing ships(1 is ship, 0 is empty)
-        int[][] field2 = new int[SIZE][SIZE];//~ for the 2nd player
         int k = 0; //int used for making fixed number of cycles
         int l = 0; //int used for making fixed number of cycles
         while (k < SIZE)//cycle that fills all the places with 0
         {
-            while (l < SIZE) {
-                field1[l][k] = 0;
+            while (l < SIZE)
+            {
+                field[l][k] = 0;
                 l++;
             }
             l = 0;
             k++;
         }
-
-
         Random random = new Random();
         k = 0; //int used for making fixed number of cycles
-        int s1 = 3; //quantity of 1x1(small) ships
-        int m1 = 2; //quantity of 1x2(medium) ships
-        int h1 = 1; //quantity of 1x3(huge) ships
-        int s2 = 3; //quantity of 1x1(small) ships
-        int m2 = 2; //quantity of 1x2(medium) ships
-        int h2 = 1; //quantity of 1x3(huge) ships
-        while (k < s1)
+        int s = 3; //quantity of 1x1(small) ships
+        while (k < s)
         {
             int r1 = Math.abs(random.nextInt()) % (SIZE); //random int r1(heights) from 0 to SIZE - 1
             int r2 = Math.abs(random.nextInt()) % (SIZE); //random int r2(width) from 0 to SIZE - 1
-            if ( areNeighborsFree(r1, r2, field1) == true)
+            if (areNeighborsFree(r1, r2, field) == true)
             {
-                if(field1[r1][r2] != 1)
-                {
-                    field1[r1][r2] = 1;
-                    ++k;
-                }
+                field[r1][r2] = 1;
+                ++k;
             }
         }
-        k = 0;
-        while (k < s2)
-        {
-            int r1 = Math.abs(random.nextInt()) % (SIZE); //random int r1(heights) from 0 to SIZE - 1
-            int r2 = Math.abs(random.nextInt()) % (SIZE); //random int r2(width) from 0 to SIZE - 1
-            if ( areNeighborsFree(r1, r2, field2) == true)
-            {
-                if(field2[r1][r2] != 1)
-                {
-                    field2[r1][r2] = 1;
-                    ++k;
-                }
-            }
-        }
-
         k = 0; //Unit test
         l = 0;
-        System.out.println("###########---FIELD1---###########");
+        /*
+        System.out.println("###########---FIELD---###########");
         while (k < SIZE)
         {
-            while (l < SIZE) {
-                System.out.print(Integer.toString(field1[l][k]) + " ");
+            while (l < SIZE)
+            {
+                System.out.print(Integer.toString(field[l][k]) + " ");
                 l++;
             }
             k++;
             l = 0;
             System.out.println(" ");
-
         }
-        k = 0; //Unit test
-        l = 0;
-        System.out.println("###########---FIELD2---###########");
-        while (k < SIZE)
-        {
-            while (l < SIZE) {
-                System.out.print(Integer.toString(field2[l][k]) + " ");
-                l++;
-            }
-            k++;
-            l = 0;
-            System.out.println(" ");
-
-        }
-
-
-
+        */
     }
 
     static boolean areNeighborsFree (int row, int col, int [][] field)
@@ -110,15 +97,12 @@ public class Field
         {
             int dR = neighbors[i][0];
             int dC = neighbors[i][1];
-
             if(isValidCoord (row + dR, col + dC))
             {
-
                 if(field[row + dR][col + dC] == 1)
                 {
                     return false;
                 }
-
             }
         }
         return true;
@@ -131,7 +115,7 @@ public class Field
 
     private static void initNeighbors()
     {
-        neighbors = new int [8][];
+        neighbors = new int [9][];
         neighbors [0] = new int [] {-1, -1};
         neighbors [1] = new int [] {-1, 0};
         neighbors [2] = new int [] {-1, 1};
@@ -140,5 +124,6 @@ public class Field
         neighbors [5] = new int [] {1, 0};
         neighbors [6] = new int [] {1, -1};
         neighbors [7] = new int [] {0, -1};
+        neighbors [8] = new int [] {0, 0};
      }
 }
--- java project/src/battlesea/Main.java
+++ java project/src/battlesea/Main.java
@@ -10,33 +10,53 @@ public class Main
     {
         boolean victory = false;
         BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
- /*     char[][] RField1 = new char[SIZE][SIZE];
-        char[][] RField2 = new char[SIZE][SIZE];
         Field f1 = new Field();
+        f1.fillFieldByRandom();
         Field f2 = new Field();
-       while (k < SIZE)//cycle that fills all the places with ~
+        int k = 0;//int used for making fixed number of cycles
+        int l = 0;//int used for making fixed number of cycles
+        int s = f1.getSmallShips();
+        int SIZE = f1.getSize();
+        int x = 0;
+        int y = 0;
+        while(k < s)
         {
-            while (l < SIZE) {
-                RField1[l][k] = '~';
-                l++;
-            }
-            l = 0;
+            System.out.println("Write the x, y coordinate of the cell you want"
+                    + " to place a small ship in");
+            x = Integer.valueOf(br.readLine());
+            y = Integer.valueOf(br.readLine());
+            f2.setCell(x, y, 1);
             k++;
         }
+        System.out.println("");
+        char[][] RField1 = new char[SIZE][SIZE];
+        char[][] RField2 = new char[SIZE][SIZE];
+        k = 0;
+        l = 0;
         while (k < SIZE)//cycle that fills all the places with ~
         {
             while (l < SIZE) {
+                RField1[l][k] = '~';
                 RField2[l][k] = '~';
                 l++;
             }
             l = 0;
             k++;
         }
- */
-        String decision = br.readLine();
-        String[] coordinates = decision.split(" ");
-        int x = Integer.valueOf(coordinates[0]);
-        int y = Integer.valueOf(coordinates[1]);
+        k = 0;
+        l = 0;
+        System.out.println("###########---FIELD---###########");
+        while (k < SIZE)
+        {
+            while (l < SIZE)
+            {
+                System.out.print(RField1[l][k] + " ");
+                l++;
+            }
+            k++;
+            l = 0;
+            System.out.println(" ");
+        }
     }
     
 }
